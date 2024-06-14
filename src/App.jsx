394ecee1e781b69e59ab60a65b2826407513b800
Hevertton Nunes de Oliveira <85510567+HeverttonNoliveira@users.noreply.github.com>
2024-06-14 09:35:01 -0300

    navigation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native';
+
+const Stack = createStackNavigator();
+export default props => {
+    return(
+        <SafeAreaView>
+            <NavigationContainer>
+                
+            </NavigationContainer>
+        </SafeAreaView>
+    )
+}
