239aabe01261d2bd81d82e6b6677120c40adacba
Hevertton Nunes de Oliveira <85510567+HeverttonNoliveira@users.noreply.github.com>
2024-06-14 09:02:11 -0300

    Finalização do Criar conta

--- src/view/CriarConta.jsx
+++ src/view/CriarConta.jsx
@@ -1,47 +1,69 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text, Image, View } from "react-native";
+import { StyleSheet, SafeAreaView, Text, Image, View, Pressable } from "react-native";
 import { TextInput } from "react-native-paper";
 
 import comonStyles from "../comonStyles";
 import logo from '../../assets/imgs/logoRight.png'
 
+import Icon from "react-native-vector-icons/Ionicons";
+
 export default props => {
-    const [text, onChangeText] = React.useState('Useless Text');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
     return (
         <SafeAreaView style={style.container}>
-            <View style={style.content}>
-                <Image source={logo} />
 
+            <View style={style.image}>
+                <Image source={logo} />
+            </View>
+
+            <View style={style.content}>
                 <View style={style.contentInput}>
                     <Text style={style.labelInput}>E-mail *</Text>
                     <TextInput
+                        mode="outlined"
                         placeholder="Digite seu email"
                         style={style.input}
-                        onFocus={onChangeText}
-                        value={text}
+                        onFocus={setEmail}
+                        value={email}
                     />
                 </View>
+
                 <View style={style.contentInput}>
                     <Text style={style.labelInput}>Senha *</Text>
                     <TextInput
-                        placeholder="Digite seu email"
+                        mode="outlined"
+                        placeholder="Digite sua senha"
                         style={style.input}
-                        onFocus={onChangeText}
-                        value={text}
+                        onFocus={() => setPassword({ isActive: true, })}
+                        value={password}
                     />
                 </View>
+
                 <View style={style.contentInput}>
                     <Text style={style.labelInput}>Confirma senha *</Text>
                     <TextInput
-                        placeholder="Digite seu email"
+                        mode="outlined"
+                        placeholder="Confirme sua senha"
                         style={style.input}
-                        onFocus={onChangeText}
-                        value={text}
+                        onFocus={setPassword}
+                        value={password}
                     />
                 </View>
             </View>
 
+            <View style={{flex:2 , gap:4, alignItems:"center"}}>
+                <Pressable style={style.button}>
+                    <Text style={style.buttonText}>Criar Conta</Text>
+                </Pressable>
+                <Text> ou entre </Text>
+                <Pressable style={style.buttonGoogle}>
+                    <Icon name="logo-google" size={18} color="#5A19FF"/>
+                    <Text style={style.buttonTextGoogle}>Google</Text>
+                </Pressable>
+                <Text> Já tem conta? Entrar </Text>
+            </View>
 
         </SafeAreaView>
     )
@@ -50,12 +72,17 @@ export default props => {
 const style = StyleSheet.create(
     {
         container: {
-            flex: 1
+            flex: 1,
+            padding: 16,
+        },
+
+        image:{
+            alignItems:"center"
         },
 
         content: {
-            padding: 16,
-            justifyContent: "center",
+            flex: 7,
+            justifyContent: "flex-start",
             alignItems: "center",
             gap: 24
         },
@@ -74,6 +101,42 @@ const style = StyleSheet.create(
 
         labelInput: {
             fontFamily: comonStyles.fontFamily,
-        }
+        },
+
+        button:{
+            width: '100%',
+            borderRadius:8,
+            paddingTop: 16,
+            paddingBottom: 16,
+            justifyContent:"center",
+            alignItems:"center",
+            backgroundColor: comonStyles.colors.primary,
+        },
+
+        buttonGoogle:{
+            gap: 8,
+            flexDirection:"row",
+            width: '100%',
+            borderRadius:8,
+            paddingTop: 16,
+            paddingBottom: 16,
+            justifyContent:"center",
+            alignItems:"center",
+            borderColor: comonStyles.colors.primary,
+            borderWidth:1
+        },
+
+        buttonText:{
+            fontSize: 14,
+            fontWeight:'700',
+            color:'#FFF'
+        },
+
+        buttonTextGoogle:{
+            fontSize: 14,
+            fontWeight:'700',
+            color:'#5A19FF'
+        },
+        
     }
 )
