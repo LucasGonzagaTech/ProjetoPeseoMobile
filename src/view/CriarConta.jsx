be5170dccca2daac5c985208c02a67df0f996917
Hevertton Nunes de Oliveira <85510567+HeverttonNoliveira@users.noreply.github.com>
2024-06-20 09:38:38 -0300

    finalização da criar conta

--- src/view/CriarConta.jsx
+++ src/view/CriarConta.jsx
@@ -1,171 +1,179 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text, Image, View, Pressable, StatusBar } from "react-native";
-import { TextInput } from "react-native-paper";
+import { StyleSheet, SafeAreaView, Text, Image, View, Pressable, StatusBar, KeyboardAvoidingView } from "react-native";
+import {TextInput } from "react-native-paper";
 import comonStyles from "../comonStyles";
 import logo from '../../assets/imgs/logoRight.png';
 import Icon from "react-native-vector-icons/Ionicons";
 
 export default props => {
-
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [confirmPassword, setConfirmPassword] = React.useState('');
-    const [error, setError] = React.useState('');
 
-    //Ocultar e mostra senha.
+    const [error, setError] = React.useState('');
     const [showPassword, setShowPassword] = React.useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
-
-    //Confirmação de senha.
-    const validatePassword = () => {
-        // if (password.length < 8) {
-        //     setError('A senha deve ter pelo menos 8 caracteres.');
-        //     return false;
-        // }
-        if (password !== confirmPassword) {
-            setError('As senhas não coincidem.');
-            return false;
-        }
-        setError('');
-        return true;
-    };
-
-    const handleSignUp = () => {
-        if (validatePassword()) {
-            // Lógica para criar a conta, como uma chamada de API
-            console.log('Conta criada com sucesso!');
-        }
-    };
-
-    return (
-        <SafeAreaView style={style.container}>
-            <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
-            <View style={style.image}>
-                <Image source={logo} />
-            </View>
-
-            <View style={style.content}>
-                <View style={style.contentInput}>
-                    <Text style={style.labelInput}>E-mail *</Text>
-                    <TextInput
-                        mode="outlined"
-                        placeholder="Digite seu email"
-                        style={style.input}
-                        onChangeText={setEmail}
-                        value={email}
-                    />
+    
+        const validatePassword = () => {
+            if (password !== confirmPassword) {
+                setError('As senhas não coincidem.');
+                return false;
+            }
+            setError('');
+            return true;
+        };
+    
+        const handleSignUp = () => {
+            if (validatePassword()) {
+                //ligação com api
+                console.log('Conta criada com sucesso!');
+            }
+        };
+    
+        const areInputsFilled = () => {
+            return email.trim() !== '' && password.trim() !== '' && confirmPassword.trim() !== '';
+        };
+    
+        return (
+            <SafeAreaView style={styles.container}>
+                <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
+                <View style={styles.image}>
+                    <Image source={logo} />
                 </View>
 
-                <View style={style.contentInput}>
-                    <Text style={style.labelInput}>Senha *</Text>
-                    <TextInput
-                        mode="outlined"
-                        placeholder="Digite sua senha"
+                <View style={styles.content}>
+                    <InputField 
+                        label="E-mail *" 
+                        placeholder="Digite seu email" 
+                        value={email} 
+                        onChangeText={setEmail} 
+                    />
+                    <InputField 
+                        label="Senha *" 
+                        placeholder="Digite sua senha" 
+                        value={password} 
+                        onChangeText={setPassword} 
                         secureTextEntry={!showPassword}
-                        style={style.input}
-                        onChangeText={setPassword}
-                        right={<TextInput.Icon 
-                            icon={showPassword ? "eye" : "eye-off"} 
-                            onPress={() => setShowPassword(!showPassword)} />
-                        }
-                        value={password}
+                        icon={showPassword ? "eye" : "eye-off"} 
+                        onPress={() => setShowPassword(!showPassword)} 
                     />
-                </View>
-
-                <View style={style.contentInput}>
-                    <Text style={style.labelInput}>Confirma senha *</Text>
-                    <TextInput
-                        mode="outlined"
-                        placeholder="Confirme sua senha"
+                    <InputField 
+                        label="Confirma senha *" 
+                        placeholder="Confirme sua senha" 
+                        value={confirmPassword} 
+                        onChangeText={setConfirmPassword} 
                         secureTextEntry={!showConfirmPassword}
-                        style={style.input}
-                        onChangeText={setConfirmPassword}
-                        right={<TextInput.Icon 
-                            icon={showConfirmPassword ? "eye" : "eye-off"} 
-                            onPress={() => setShowConfirmPassword(!showConfirmPassword)} />
-                        }
-                        value={confirmPassword}
+                        icon={showConfirmPassword ? "eye" : "eye-off"} 
+                        onPress={() => setShowConfirmPassword(!showConfirmPassword)} 
                     />
+                    {error ? <Text style={styles.error}>{error}</Text> : null}
                 </View>
-                {error ? <Text style={style.error}>{error}</Text> : null}
-            </View>
-
-            <View style={{ flex: 2, gap: 4, alignItems: "center" }}>
-                <Pressable style={style.button} onPress={handleSignUp}>
-                    <Text style={style.buttonText}>Criar Conta</Text>
-                </Pressable>
-                <Text> ou entre </Text>
-                <Pressable style={style.buttonGoogle}>
-                    <Icon name="logo-google" size={18} color="#5A19FF" />
-                    <Text style={style.buttonTextGoogle}>Google</Text>
-                </Pressable>
-                <Text> Já tem conta? Entrar </Text>
-            </View>
-        </SafeAreaView>
+                <View style={styles.buttonContainer}>
+                    <Pressable 
+                        style={({ pressed }) => [
+                            {
+                                backgroundColor: !areInputsFilled() ? '#d3d3d3' : pressed ? '#3700B3' : comonStyles.colors.primary,
+                                opacity: !areInputsFilled() ? 0.7 : 1,
+                            },
+                            styles.button
+                        ]}
+                        onPress={handleSignUp}
+                        disabled={!areInputsFilled()}
+                    >
+                        <Text style={styles.buttonText}>Criar Conta</Text>
+                    </Pressable>
+                    <Text> ou entre </Text>
+                    <Pressable style={styles.buttonGoogle}>
+                        <Icon name="logo-google" size={18} color="#5A19FF" />
+                        <Text style={styles.buttonTextGoogle}>Google</Text>
+                    </Pressable>
+                    <Text> Já tem conta? Entrar </Text>
+                </View>
+            </SafeAreaView>
+        );
+    };
+    
+    const InputField = ({ label, placeholder, value, onChangeText, secureTextEntry, icon, onPress }) => (
+             <View style={styles.contentInput}>
+            <Text style={styles.labelInput}>{label}</Text>
+            <TextInput
+                mode="outlined"
+                placeholder={placeholder}
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={value}
+                secureTextEntry={secureTextEntry}
+                right={icon ? <TextInput.Icon icon={icon} onPress={onPress} /> : null}
+            />
+        </View>
+       
     );
-};
-
-const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    },
-    image: {
-        marginTop: 42,
-        alignItems: "center"
-    },
-    content: {
-        flex: 5,
-        justifyContent: "flex-start",
-        alignItems: "center",
-        gap: 24
-    },
-    contentInput: {
-        width: '100%',
-        gap: 8
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#CCC4D8',
-        backgroundColor: 'none',
-        borderRadius: 8
-    },
-    labelInput: {
-        fontFamily: comonStyles.fontFamily,
-    },
-    error: {
-        color: 'red',
-        marginTop: 8,
-    },
-    button: {
-        width: '100%',
-        borderRadius: 8,
-        paddingTop: 16,
-        paddingBottom: 16,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: comonStyles.colors.primary,
-    },
-    buttonGoogle: {
-        gap: 8,
-        flexDirection: "row",
-        width: '100%',
-        borderRadius: 8,
-        paddingTop: 16,
-        paddingBottom: 16,
-        justifyContent: "center",
-        alignItems: "center",
-        borderColor: comonStyles.colors.primary,
-        borderWidth: 1
-    },
-    buttonText: {
-        fontSize: 14,
-        fontWeight: '700',
-        color: '#FFF'
-    },
-    buttonTextGoogle: {
-        fontSize: 14,
-        fontWeight: '700',
-        color: '#5A19FF'
-    },
-});
+    
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            padding: 16,
+        },
+        image: {
+            marginTop: 42,
+            alignItems: "center"
+        },
+        content: {
+            flex: 5,
+            justifyContent: "flex-start",
+            alignItems: "center",
+            gap: 24
+        },
+        contentInput: {
+            width: '100%',
+            gap: 8
+        },
+        input: {
+            borderWidth: 1,
+            borderColor: '#CCC4D8',
+            backgroundColor: 'none',
+            borderRadius: 8
+        },
+        labelInput: {
+            fontFamily: comonStyles.fontFamily,
+        },
+        error: {
+            color: 'red',
+            marginTop: 8,
+        },
+        buttonContainer: {
+            flex: 2,
+            gap: 4,
+            alignItems: "center"
+        },
+        button: {
+            width: '100%',
+            borderRadius: 8,
+            paddingTop: 16,
+            paddingBottom: 16,
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        buttonGoogle: {
+            gap: 8,
+            flexDirection: "row",
+            width: '100%',
+            borderRadius: 8,
+            paddingTop: 16,
+            paddingBottom: 16,
+            justifyContent: "center",
+            alignItems: "center",
+            borderColor: comonStyles.colors.primary,
+            borderWidth: 1
+        },
+        buttonText: {
+            fontSize: 14,
+            fontWeight: '700',
+            color: '#FFF'
+        },
+        buttonTextGoogle: {
+            fontSize: 14,
+            fontWeight: '700',
+            color: '#5A19FF'
+        },
+    });
